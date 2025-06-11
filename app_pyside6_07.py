eeff9b1a32260702b592233896d65297c21126c3
inoricode28 <inori237@gmail.com>
2025-06-11 02:03:33 -0500

    resize

--- app_pyside6_07.py
+++ app_pyside6_07.py
@@ -1,13 +1,26 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtGui import QFont, QPixmap
+from PySide6.QtCore import Qt
+from pathlib import Path
 import sys
 
+def absPath(file):
+    return str(Path(__file__).parent.absolute() / file)
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()        
-        self.resize(480,320)
-        etiqueta=QLabel("Soy una etiqueta")
+        self.setWindowTitle("Pyside6")       
+        self.resize(1280,720)
+
+        etiqueta=QLabel("Soy una etiqueta")       
+        fuente=QFont("Comic Sans MS", 24)
+        etiqueta.setFont(fuente)
+        etiqueta.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) #Centra en Vertical y Horizontal
+        imagen= QPixmap(absPath("img.jpg"))
+        etiqueta.setPixmap(imagen)
+        etiqueta.setScaledContents(True)
         self.setCentralWidget(etiqueta)
-        
+
 
 
 
